e3e0ebbae9f0b31cf05c16da7790ed257b927d20
GINOGALOIS <020101702H@uandina.edu.pe>
2022-08-05 21:51:28 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.gino020101702h {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.gino020101702h to javafx.fxml;
+    exports com.example.gino020101702h;
+}
